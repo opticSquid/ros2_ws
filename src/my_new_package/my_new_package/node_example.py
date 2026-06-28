07f48cd4b7217b8c396a6bb938344780372f6f7c
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-06-28 11:39:42 +0530

    Feat: Creating a custom node and extracting logic inside it

--- src/my_new_package/my_new_package/node_example.py
+++ src/my_new_package/my_new_package/node_example.py
@@ -1,10 +1,14 @@
 import rclpy
 from rclpy.node import Node
 
+class CustomNode(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.get_logger().info("hello world!")
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("example_node_name")
-    node.get_logger().info("hello world!")
+    node = CustomNode("example_node_name")
     # keep looping until keyboard interrupt
     rclpy.spin(node)
     rclpy.shutdown()
